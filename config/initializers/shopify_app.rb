d3f03074383862b09e0ab22aa692ad709c3e9bcb
brightmanforever022 <brightmanforever@outlook.com>
2020-01-23 17:11:43 -0500

    change env and scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,8 +3,9 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products,write_products,read_themes,write_themes,read_customers,write_customers,write_draft_orders,read_draft_orders,read_orders,write_orders,read_price_rules" # Consult this page for more scope options:
-                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  # config.scope = "read_products,write_products,read_themes,write_themes,read_customers,write_customers,write_draft_orders,read_draft_orders,read_orders,write_orders,read_price_rules,read_discounts,read_product_listings,write_product_listings" # Consult this page for more scope options:
+  config.scope = "read_products,write_products,read_themes,write_themes,read_customers,write_customers,write_draft_orders,read_draft_orders,read_price_rules,read_discounts,read_product_listings" # Consult this page for more scope options:
+  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.api_version = ENV['SHOPIFY_API_VERSION']
   config.session_repository = Shop
